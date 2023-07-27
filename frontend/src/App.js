3bad8820ba3d42ae8fe6f48fdb9d28b21a3bf93d
robinshrestha12 <robinshrestha.maple@gmail.com>
2023-07-27 17:10:14 -0400

    rolled to initial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
-import Home from '../src/components/Home';
 
 function App() {
   return (
     <div className="App">
-   <h1>Easylife technology website making</h1>
-   <Home />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
